30096a5690c1aa94d84fc2a7598f643f70c23373
FabianFle <fabianflegler1@yahoo.de>
2023-02-22 13:07:21 +0100

    added img an sliedshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FabianFlegler';
+export class AppComponent implements OnInit {
+  img = ['codeImg.jpg', 'styleImg.jpg', 'workImg.jpg'];
+  currenImg = 0;
+  showImg = true
+
+  ngOnInit() {
+    this.updateImg();
+  }
+
+  updateImg() {
+    setInterval(() => {
+      this.currenImg++;
+      this.currenImg = this.currenImg % this.img.length;
+      this.showImg = false;
+
+      setTimeout(() => {
+        this.showImg = true;
+      }, 10);
+    }, 8000);
+  }
+
 }
